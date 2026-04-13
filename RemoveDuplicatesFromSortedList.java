f4a4135c4d879afb722eab16c37095dcd87c5e12
Rimjhim Kumari <krimjhim464@gmail.com>
2026-04-13 20:16:45 +0530

    Add files via upload
    Leetcode 82: -  Remove duplicates from sorted list II

---
+++ RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while(temp != null){
+            if(temp.next != null && temp.val == temp.next.val){
+                int value = temp.val;
+                while(temp != null && temp.val == value){
+                    temp = temp.next;
+                }
+                if(prev != null) prev.next = temp;
+                else head = temp;
+            }else{
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
