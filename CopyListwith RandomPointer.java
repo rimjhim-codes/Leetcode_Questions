8e5eba0188544d978855cabf1cbe7469b3a48508
Rimjhim Kumari <krimjhim464@gmail.com>
2026-04-13 20:13:11 +0530

    Add files via upload
    138. Copy List with Random Pointer

---
+++ CopyListwith RandomPointer.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+
+        if(head==null) return null;
+
+        Map<Node, Node> mpp = new HashMap<>();
+        Node temp = head;
+
+        while(temp != null){
+            Node newNode = new Node(temp.val);
+            mpp.put(temp, newNode);
+            temp = temp.next;
+        }
+
+        temp = head;
+
+        while(temp != null){
+            Node copyNode = mpp.get(temp);
+            copyNode.next = mpp.get(temp.next);
+            copyNode.random = mpp.get(temp.random);
+            temp = temp.next;
+        }
+
+        return mpp.get(head);
+    }
+}
